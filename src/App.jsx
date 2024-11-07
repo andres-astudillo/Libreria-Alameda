ca3be213b868e0e16755422aff5d0a77805aed07
Andrés Astudillo <andresdiastullo@gmail.com>
2024-11-07 17:19:03 -0300

    Revisando errores

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import './App.css'
-import ProductList from './App/ItemListContainer/ItemList/ItemList';
+import './App.css';
+import ItemList from './App/ItemListContainer/ItemList/ItemList';
 import ItemDetail from './App/ItemDetailContainer/ItemDetail/ItemDetail';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import AddItem from './App/ItemDetailContainer/ItemDetail/AddItem';
@@ -7,26 +7,31 @@ import NavBar from './App/NavBar/NavBar';
 import Footer from './App/Footer/Footer';
 import UpdateItem from './App/ItemDetailContainer/ItemDetail/UpdateItem';
 import { CartProvider } from './context/CartContext';
+import CartModal from './App/Cart/CartModal';
+import { useState } from 'react';
 
 function App() {
+    const [isCartOpen, setIsCartOpen] = useState(false);
 
-  return (
-    <div className="flex flex-col min-h-screen">
-      <CartProvider>
-        <Router>
-          < NavBar />
-          <Routes>
-            <Route path="/" element={< ProductList />} />
-            <Route path="/detalledelproducto/:id" element={< ItemDetail />} />
-            <Route path="/nuevoproducto/" element={< AddItem />} />
-            <Route path="/editarproducto/:id" element={< UpdateItem />} />
-          </Routes>
-          < Footer />
-        </Router>
-      </CartProvider>
-    </div>
-  );
+    const toggleCartModal = () => {
+        setIsCartOpen(!isCartOpen);
+    };
+
+    return (
+        <CartProvider>
+            <Router>
+                <NavBar toggleCartModal={toggleCartModal} />
+                <Routes>
+                    <Route path="/" element={<ItemList />} />
+                    <Route path="/detalledelproducto/:id" element={<ItemDetail />} />
+                    <Route path="/nuevoproducto/" element={<AddItem />} />
+                    <Route path="/editarproducto/:id" element={<UpdateItem />} />
+                </Routes>
+                <Footer />
+                <CartModal isOpen={isCartOpen} toggleCartModal={toggleCartModal} />
+            </Router>
+        </CartProvider>
+    );
 }
 
-export default App
-
+export default App;
